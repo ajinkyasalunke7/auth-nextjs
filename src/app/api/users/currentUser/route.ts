972ce1282d9c275754373939bf60172a6518157d
ajinkyasalunke7 <ajinkyasalunkke90@outlook.com>
2024-01-22 19:55:58 +0530

    log fixed

--- src/app/api/users/currentUser/route.ts
+++ src/app/api/users/currentUser/route.ts
@@ -8,7 +8,7 @@ export async function GET(request: NextRequest) {
     try {
         const userId = await getDataFromToken(request);
         const user = await User.findOne({ _id: userId }).select("-password");
-        console.log("11111111111111111111", user.username);
+        //console.log("11111111111111111111", user.username);
         return NextResponse.json({
             message: "User found",
             data: user,
