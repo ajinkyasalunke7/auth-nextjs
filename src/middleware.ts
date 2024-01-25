f2b7ebb6918c337618f75e3dd74d1d86514fe23a
ajinkyasalunke7 <ajinkyasalunkke90@outlook.com>
2024-01-25 20:13:44 +0530

    profile route fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -21,5 +21,12 @@ export function middleware(request: NextRequest) {
 // };
 
 export const config = {
-    matcher: ["/", "/profile", "/login", "/signup", "/verifyemail"],
+    matcher: [
+        "/",
+        "/profile",
+        "/login",
+        "/signup",
+        "/verifyemail",
+        "/profile/:path*",
+    ],
 };
